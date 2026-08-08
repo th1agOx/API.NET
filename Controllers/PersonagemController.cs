3f94e62ce136177874703f2f602dc81fc95f9e92
th1agOx <thdarochabp@gmail.com>
2026-08-08 17:13:51 -0300

    CRUDs de interação com banco criado Versão via controllers

--- Controllers/PersonagemController.cs
+++ Controllers/PersonagemController.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjetoApi1.Data;
 using ProjetoApi1.Models;
 
@@ -20,12 +17,76 @@ namespace ProjetoApi1.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddPersonagem(Personagem personagem)
+        public async Task<IActionResult> AddPersonagem([FromBody] Personagem personagem)
         {
+            if (ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             _appDbContext.Personagens.Add(personagem);
             await _appDbContext.SaveChangesAsync();
 
+            return Created("Personagem adicionar com sucesso!", personagem);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Personagem>>> GetPersonagens()
+        {
+            var personagens = await _appDbContext.Personagens.ToListAsync();
+
+            return Ok(personagens);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Personagem>> GetPersonagemById(int id)
+        {
+            var personagem = await _appDbContext.Personagens.FindAsync(id);
+
+            if (personagem == null)
+            {
+                return NotFound("Personagem não localizado no banco");
+            }
+
             return Ok(personagem);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> UpdatePersonagemById(int id, [FromBody] Personagem personagemAtt)
+        {
+            var personagemExistente = await _appDbContext.Personagens.FindAsync(id);
+
+            if (personagemExistente == null)
+            {
+                return NotFound("Personagem não localizado no banco");
+            }
+
+            _appDbContext.Entry(personagemExistente).CurrentValues.SetValues
+            (personagemAtt);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return StatusCode(201, personagemExistente);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeletePersonagem(int id)
+        {
+            var personagemExistente = await _appDbContext.Personagens.FindAsync(id);
+
+            if (personagemExistente == null)
+            {
+                NotFound("Personagem não localizado no banco");
+            }
+
+            _appDbContext.Personagens.Remove(personagemExistente);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok("Personagem deletado !");
         }
     }
 }
--- Models/Personagem.cs
+++ Models/Personagem.cs
@@ -1,16 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoApi1.Models
 {
     public class Personagem
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Nome é um campo obrigatório")]
+        [MaxLength(50, ErrorMessage = "Nome não pode exceder ")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Tipo é um campo obrigatório")]
+        [MaxLength(50, ErrorMessage = "Tipo precisa ter ")]
         public string Tipo { get; set; }
     }
 }
